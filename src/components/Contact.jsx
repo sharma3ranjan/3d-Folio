da5551e1fa3b98aad0a825370a3a3dd77d7be4fb
Ranjan <just.think2019@gmail.com>
2023-03-23 14:55:39 +0530

    Some Updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -35,8 +35,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_n3pml2k',
+        'template_d4vww7s',
         {
           from_name: form.name,
           to_name: "Ranjan Sharma",
@@ -44,7 +44,7 @@ const Contact = () => {
           to_email: "just.think2003@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        '3gwsA6EqqDRScOC_R'
       )
       .then(
         () => {
