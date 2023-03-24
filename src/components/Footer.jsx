6b34b8150808031e42b20c2ae01400a1edd951b6
Ranjan <just.think2019@gmail.com>
2023-03-24 18:10:53 +0530

    webkit update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ import { SectionWrapper } from '../hoc'
 const Footer = () => {
     return (
         <div>
-            <p className='text-center pb-2 sm:text-sm'>Suggest Any Changes by Filling Above Form❤️</p>
-            <p className='text-center'>
-                Built with lots of motivation using React and 3JS by Ranjan Sharma 💛
+            <p className='text-center pb-2 text-xs text-white'>Suggest Any Changes by Filling Above Form❤️</p>
+            <p className='text-center text-sm text-white'>
+                Built with lots of motivation using React and 3JS by <a className='text-green-600' href="mailto:just.think2003@gmail.com">Ranjan Sharma</a> 💛
             </p>
         </div>
     )
